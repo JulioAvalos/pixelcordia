3513c1cd15e765e13f4d3ffe6a936e0ea0ed40fa
JulioAvalos <julioavalos2@msn.com>
2020-07-27 19:40:20 -0600

    discord tutorial bot - passing down commands

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+
+    // client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    console.log(args);
+    const command = args.shift().toLowerCase();
+    console.log(command);
+    // the rest of your code
+
     if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong.');
     } else if (message.content.startsWith(`${prefix}beep`)) {
@@ -19,6 +29,40 @@ client.on('message', message => {
     else if (message.content === `${prefix}user-info`) {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     }       
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+        message.channel.send(`First argument: ${args[0]}`);
+    }
+    else if (command === 'kick') {
+        // grab the "first" mentioned user from the message
+        // this will return a `User` object, just like `message.author`
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
+        }        
+        const taggedUser = message.mentions.users.first();
+    
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+        message.channel.send(` woah woah que toxico!`);
+    }    
+
+    else if (command === 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+        }
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+        });
+    
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
+    }
 });
 
 client.login(token);
